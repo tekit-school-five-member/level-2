f7a8e5951be1e50a42c39c8203966eecb6c993cb
unknown <taewoo1212@gmail>
2023-12-20 00:35:59 +0900

    remove .git files

---
+++ level-2/main.js
@@ -0,0 +1,11 @@
+/**
+ * 목표!!
+ * 1. 드래그 시작시 fill엘리먼트에 hold 클래스 추가
+ * 2. fill엘리먼트 클래스를 invisible로 변경 (hint: setimeout 함수의 비동기 처리 기능 사용)
+ * 3. 드래그가 끝나면 empty엘리먼트에 fill엘리먼트를 자식 노드로 넣기
+ * 4. 마우스가 호버되는 empty엘리먼트에 hovered 클래스 추가
+ * 5. 마우스가 호버가 끝나면 empty엘리먼트에 hovered 클래스 제거
+ */
+
+// test
+//test2
